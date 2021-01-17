78baab7cf9cb53f5187edb29bee96b6f96dbda7e
Nicolás Molina <nicolas.molina@tecso.coop>
2021-01-17 10:40:22 -0300

    Api funcionando, test Ok.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 var express = require('express') 
 var app = express()       
-var bodyParser = require('body-parser')        
+var bodyParser = require('body-parser')
+const request = require('supertest');
 
 var port = 3000;
 
@@ -16,77 +17,55 @@ app.use('/v1', router)
 app.listen(port)
 console.log('API escuchando en el puerto ' + port)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Tests:
-// request(app)
-//     .get('/v1/location')
-//     .expect('Content-Type', /json/)
-//     .expect(200)
-//     .end(function(err, res) {
-//         if(res)
-//             console.log("Test v1/location OK!")
-//         if (err) throw err;
-//     });
+request(app)
+    .get('/v1/location')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function(err, res) {
+        if(res)
+            console.log("Test v1/location OK!")
+        if (err) throw err;
+    });
  
-// request(app)
-//     .get('/v1/current')
-//     .expect('Content-Type', /json/)
-//     .expect(200)
-//     .end(function(err, res) {
-//         if(res)
-//             console.log("Test /v1/current OK!")
-//         if (err) throw err;
-//     });
+request(app)
+    .get('/v1/current')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function(err, res) {
+        if(res)
+            console.log("Test /v1/current OK!")
+        if (err) throw err;
+    });
  
-// request(app)
-//     .get('/v1/current/rosario')
-//     .expect('Content-Type', /json/)
-//     .expect(200)
-//     .end(function(err, res) {
-//         if(res)
-//             console.log("Test /v1/current/rosario OK!")
-//         if (err) throw err;
-// }); 
+request(app)
+    .get('/v1/current/rosario')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function(err, res) {
+        if(res)
+            console.log("Test /v1/current/rosario OK!")
+        if (err) throw err;
+}); 
 
-// request(app)
-//     .get('/v1/forecast')
-//     .expect('Content-Type', /json/)
-//     .expect(200)
-//     .end(function(err, res) {
-//         if(res)
-//             console.log("Test /v1/forecast OK!")
-//         if (err) throw err;
-// }); 
+request(app)
+    .get('/v1/forecast')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function(err, res) {
+        if(res)
+            console.log("Test /v1/forecast OK!")
+        if (err) throw err;
+}); 
 
-// request(app)
-//     .get('/v1/forecast/cordoba')
-//     .expect('Content-Type', /json/)
-//     .expect(200)
-//     .end(function(err, res) {
-//         if(res)
-//             console.log("Test /v1/forecast/cordoba OK!")
-//         if (err) throw err;
-//     }); 
+request(app)
+    .get('/v1/forecast/cordoba')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function(err, res) {
+        if(res)
+            console.log("Test /v1/forecast/cordoba OK!")
+        if (err) throw err;
+    }); 
 
 
